11ba162fb9daeff0fd902866e65b2c631d66f958
Shaquon <shaquonkelley@gmail.com>
2022-11-21 21:18:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,129 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [
+    valid1,
+    valid2,
+    valid3,
+    valid4,
+    valid5,
+    invalid1,
+    invalid2,
+    invalid3,
+    invalid4,
+    invalid5,
+    mystery1,
+    mystery2,
+    mystery3,
+    mystery4,
+    mystery5,
+];
+
+// Add your functions below:
+const validateCred = (creditCardList) => {
+    let sum = 0;
+    let checkDigit = creditCardList[creditCardList.length - 1];
+    for (let i = creditCardList.length - 1; i >= 0; i--) {
+        if (i === creditCardList.length - 1) {
+            sum += checkDigit;
+        }
+        // check if length of array is even
+        else if (creditCardList.length % 2 === 0) {
+            if (i % 2 === 0) {
+                let oddElement = creditCardList[i] * 2;
+                if (oddElement > 9) {
+                    oddElement = oddElement - 9;
+                }
+                sum += oddElement;
+            } else if (i % 2 !== 0) {
+                sum += creditCardList[i];
+            }
+        }
+        // check if array length is odd
+        else if (creditCardList.length % 2 !== 0) {
+            if (i % 2 !== 0) {
+                let oddElement = creditCardList[i] * 2;
+                if (oddElement > 9) {
+                    oddElement = oddElement - 9;
+                }
+                sum += oddElement;
+            } else if (i % 2 === 0) {
+                sum += creditCardList[i];
+            }
+        }
+    }
+
+    if (sum % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+/*
+const arrayOfCardNums = [
+  [4, 5, 6, 7...],
+  [5, 6, 4, 2...],
+  [6, 1, 6, 7...]
+]
+*/
+const findInvalidCards = (arrayOfCardNums) => {
+    const invalidCardNums = [];
+
+    for (let i = 0; i < arrayOfCardNums.length; i++) {
+        if (!validateCred(arrayOfCardNums[i])) {
+            invalidCardNums.push(arrayOfCardNums[i]);
+        }
+    }
+    return invalidCardNums;
+};
+
+const invalidCardCompanies = (invalidCardNums) => {
+    const invalidCompanies = [];
+
+    for (let i = 0; i < invalidCardNums.length; i++) {
+        // amex
+        if (invalidCardNums[i][0] === 3) {
+            invalidCompanies.push("Amex");
+        }
+        // Visa
+        else if (invalidCardNums[i][0] === 4) {
+            invalidCompanies.push("Visa");
+        }
+        // Mastercard
+        else if (invalidCardNums[i][0] === 5) {
+            invalidCompanies.push("Mastercard");
+        }
+        //Discover
+        else if (invalidCardNums[i][0] === 6) {
+            invalidCompanies.push("Discover");
+        }
+        // Company not found
+        else {
+            invalidCompanies.push("Company not found");
+        }
+    }
+    return invalidCompanies;
+};
+
+const listOfInvalidCardNums = findInvalidCards(batch);
+
+console.log(invalidCardCompanies(listOfInvalidCardNums));
